d4d96767aff980030d186077040cb0fe2158ea35
Rishabh Gupta <gupta.rishabh.abcd@gmail.com>
2019-07-23 10:49:15 +0530

    AA

--- servicedelivery/admin.py
+++ servicedelivery/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from servicedelivery.models import Transporter,SalesOrderProductPlan
+from servicedelivery.models import Transporter,SalesOrderProductPlan,PurchaseOrderProductPlan
 admin.site.register(Transporter)
 admin.site.register(SalesOrderProductPlan)
+admin.site.register(PurchaseOrderProductPlan)
 # Register your models here.
