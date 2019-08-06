ecce95c72f0fabe3934398af574bb10f307d4fe8
Rishabh Gupta <gupta.rishabh.abcd@gmail.com>
2019-08-06 10:19:44 +0530

    sdsd

--- payments/admin.py
+++ payments/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 #from .models import Payment
+from payments.models import PaymentPayable,ChequePayable
 from ordered_model.admin import OrderedModelAdmin
 # Register your models here.
 #class PaymentAdmin(OrderedModelAdmin):
     #list_display = ['vendor','amount','move_up_down_links']
 #admin.site.register(Payment,PaymentAdmin)
+admin.site.register(PaymentPayable,ChequePayable)
